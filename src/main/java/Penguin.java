1d94fcde0a4ae37c9711942e012b79fea87f1571
panjx-7339 <panjinxia06@gmail.com>
2026-01-21 22:57:36 +0800

    feat: echo user commands

--- src/main/java/Penguin.java
+++ src/main/java/Penguin.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Penguin {
     private static void start() {
         String logo = "⠀⠀⠀⠀⠀⠀⠀⣠⣠⣶⣿⣷⣿⣿⣿⣷⣷⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
@@ -19,7 +21,7 @@ public class Penguin {
                 "⠀⠀⠀⠀⠀⠀⠀⠻⢿⣿⣿⣿⣿⠿⠿⣿⡿⠿⠿⠿⠿⣿⣿⣿⠿⠟⠁⠀⠀⠀⠀";
         System.out.println("____________________________________________________");
         System.out.println("Hello! I'm Penguin.\n" + logo + "\nWhat can I do for you?");
-        System.out.println("____________________________________________________");
+        System.out.println("____________________________________________________\n");
     }
 
     private static void exit() {
@@ -28,8 +30,25 @@ public class Penguin {
         System.out.println("____________________________________________________");
     }
 
+    private static void echo(String command) {
+        System.out.println("____________________________________________________");
+        System.out.println(command);
+        System.out.println("____________________________________________________");
+    }
+
     public static void main(String[] args) {
         start();
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        // Exit conversation if user types the command "bye"
+        while (true) {
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            echo(input);
+        }
+        sc.close();
         exit();
     }
 }
